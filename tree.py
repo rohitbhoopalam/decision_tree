b6da571199cacc7dd4e2e6e9c2c95c1ae947c198
Rohit Bhoopalam <rohitbr1@gmail.com>
2015-11-11 02:06:09 -0600

    Decision tree is getting built Prediction part is remaining

--- tree.py
+++ tree.py
@@ -8,18 +8,22 @@ import math
 from collections import Counter
 
 class TNode(object):
-    NUMBER_OF_CLASSES = 10
-    CLASSES = []
-    self.parent = None
-    self.children = []
-    self.data = []
-    self.attributes = []
 
-    def __init__(self, data=[], children=[], parent=None, attributes=[]):
+    def __init__(self, data=[], attributes={}, parent=None, children=[]):
         self.parent = parent
         self.children = children
         self.data = data
-        self.attributes = attributes
+        self.attributes = attributes.copy()
+        self.name = ""
+        self.predicted_class = {}
+
+    def activeAttributes(self):
+        active_attributes = []
+        for att in self.attributes:
+            if self.attributes[att]:
+                active_attributes.append(att)
+
+        return active_attributes
 
     def splitNode(self):
         """
@@ -27,13 +31,16 @@ class TNode(object):
         """
         score = []
 
-        for i in range(len(self.attributes)):
-            score.append(self.entropy(i))
+        active_attributes = self.activeAttributes()
+        for att_index in active_attributes:
+            score.append(self.entropy(att_index))
+        print "Entropy scores", score
         min_index = score.index(min(score))
 
-        return min_index
+        return active_attributes[min_index]
 
     def attributeActualIndex(self, att_index):
+        return att_index
         #attribute index in data
         actual_attribute_index = self.attributes[att_index]
         return actual_attribute_index
@@ -93,35 +100,89 @@ class TNode(object):
             attribute_entropy += (sum_child/data_len) * child_entropy
         return attribute_entropy
 
-    def computeOfChildren(self, min_att_index):
+    def computeOfChildren(self, min_att):
         """
             Returns the number of unique elements
             Assuming those will be the total number of children
             the current node can have
         """
-        actual_attribute_index = self.attributeActualIndex(min_att_index)
+        actual_attribute_index = self.attributeActualIndex(min_att)
         _unique_types = set(self.data[:, actual_attribute_index])
         return _unique_types
+
+    def findChildrenDataAttributes(self, child, min_att):
+        actual_attribute_index = self.attributeActualIndex(min_att)
+        _data = self.data[np.where(self.data[:, actual_attribute_index] == child)]
+        self.attributes[actual_attribute_index] = False 
+        _attributes = self.attributes.copy()
+
+        return _data, _attributes
+
+    def findSplittingRequired(self):
+        classes = set(self.data[:,-1])
+        class_case = True if len(classes) > 1 else False
+        att_case = False
+        for att in self.attributes:
+            if self.attributes[att] == True:
+                att_case = True
+        
+        return class_case and att_case
 
     def findChildren(self):
         """
         """
-        min_att_index = self.splitNode(self)
+        min_att = self.splitNode()
 
-        children = self.computeOfChildren(min_att_index)
+        self.name = str(min_att)
+        children = self.computeOfChildren(min_att)
 
+        print min_att, children
         for child in children:
-            _data = []
-            _attributes = []
+            _data, _attributes = self.findChildrenDataAttributes(child, min_att)
+            print child, _data, _attributes, len(_data)
             _parent = self
-            _children = []
-            _splitting_required = findSplittingRequired()
 
-            node = TNode(_data, _attributes, _parent, _children)
+
+            node = TNode(_data, _attributes, _parent)
             self.children.append(node)
+
+            _splitting_required = node.findSplittingRequired()
+
             if _splitting_required:
                 node.findChildren()
+            else:
+                node.processClass()
+
+    def processClass(self):
+        """
+        """
+        predicted_class = Counter(self.data[:,-1])
+        class_total = 0
+        for cls in predicted_class:
+            class_total += predicted_class[cls]
+
+        for cls in predicted_class:
+            predicted_class[cls] = float(predicted_class[cls])/float(class_total)
+        
+        self.predicted_class = predicted_class
 
 if __name__ == "__main__":
+    #reading data
+    data = []
+
+    f = open("MushroomTrain.csv")
+    for all_lines in f:
+        all_lines = all_lines.split()
+        for line in all_lines:
+            line = line.split(',')
+            temp = line[1:-1]
+            temp.append(line[0])
+            temp = np.array(temp)
+            data.append(temp)
+            print temp
+    
+    data = np.array(data)
+    attributes = {0: True, 1: True, 2: True, 3: True}
     root = TNode(data, attributes, None, [])
     root.findChildren()
+
